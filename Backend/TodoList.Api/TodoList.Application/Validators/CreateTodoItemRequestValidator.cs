b770e7543a224094aa09f75caf2b58e225922cb0
Lee <rockleeyu@gmail.com>
2024-08-27 10:08:11 +1200

    refactor: implement validators for the service

---
+++ Backend/TodoList.Api/TodoList.Application/Validators/CreateTodoItemRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using TodoList.Domain;
+using TodoList.Domain.Entities;
+
+namespace TodoList.Application.Validators
+{
+    public interface ICreateTodoItemValidator : IValidator<TodoItem> { }
+
+    public class CreateTodoItemRequestValidator : AbstractValidator<TodoItem>, ICreateTodoItemValidator
+    {
+        private readonly ITodoItemRepository _todoItemRepository;
+
+        public CreateTodoItemRequestValidator(ITodoItemRepository todoItemRepository)
+        {
+            _todoItemRepository = todoItemRepository;
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MaximumLength(200).WithMessage("Description cannot exceed {MaxLength} characters.")
+                .MustAsync(HasNoDuplicateUncompletedTodoItemAsync).WithMessage("A todo item with the same description already exists.");
+        }
+
+        private async Task<bool> HasNoDuplicateUncompletedTodoItemAsync(string desc, CancellationToken cancellationToken)
+        {
+            var todoItem = await _todoItemRepository.GetAllAsync(x=>x.Description==desc && !x.IsCompleted);
+            return !todoItem.Any();
+        }
+    }
+}
---
+++ Backend/TodoList.Api/TodoList.Application/Validators/UpdateTodoItemRequestValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using TodoList.Domain;
+using TodoList.Domain.Entities;
+
+namespace TodoList.Application.Validators
+{
+    public interface IUpdateTodoItemValidator : IValidator<TodoItem> { }
+
+    public class UpdateTodoItemRequestValidator : AbstractValidator<TodoItem>, IUpdateTodoItemValidator
+    {
+        private readonly ITodoItemRepository _todoItemRepository;
+
+        public UpdateTodoItemRequestValidator(ITodoItemRepository todoItemRepository)
+        {
+            _todoItemRepository = todoItemRepository;
+
+            RuleFor(x => x.Id)
+                .NotNull().NotEqual(Guid.Empty).WithMessage("Id is invalid.")
+                .MustAsync(ExistTodoItemAsync).WithMessage("Could not find the todo item by Id '{PropertyValue}'");
+
+            RuleFor(x => x.Description)
+              .NotEmpty().WithMessage("Description is required.")
+              .MaximumLength(200).WithMessage("Description cannot exceed 200 characters.");
+        }
+
+        private async Task<bool> ExistTodoItemAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var todoItem = await _todoItemRepository.GetByIdAsync(id);
+            return todoItem != null;
+        }
+
+    }
+}
