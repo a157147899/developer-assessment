87a4cf0cc1cd1099afee1ef2fd03a3583cc40524
Lee <rockleeyu@gmail.com>
2024-08-27 10:03:26 +1200

    refactor: implement the domain layer

---
+++ Backend/TodoList.Api/TodoList.Domain/Entities/TodoItem.cs
@@ -0,0 +1,26 @@
+﻿namespace TodoList.Domain.Entities
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? CompletedDate { get; set; }
+
+        public TodoItem()
+        {
+
+        }
+
+        /// <summary>
+        /// Mark the todo item as completed
+        /// </summary>
+        public void MarkAsCompleted()
+        {
+            IsCompleted = true;
+            CompletedDate = DateTime.Now;
+        }
+
+    }
+}
---
+++ Backend/TodoList.Api/TodoList.Domain/ITodoItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using TodoList.Domain.Entities;
+
+namespace TodoList.Domain
+{
+    public interface ITodoItemRepository
+    {
+        Task<TodoItem> GetByIdAsync(Guid id);
+        Task<List<TodoItem>> GetAllAsync();
+        Task<List<TodoItem>> GetAllAsync(Expression<Func<TodoItem, bool>> predicate);
+        Task<TodoItem> AddAsync(TodoItem item);
+        Task<TodoItem> UpdateAsync(TodoItem item);
+        Task DeleteAsync(Guid id);
+    }
+
+}
