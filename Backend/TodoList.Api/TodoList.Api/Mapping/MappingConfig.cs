57386b0b237b60467b564179b4968f5bae5819d5
Lee <rockleeyu@gmail.com>
2024-08-27 10:10:14 +1200

    refactor: add Mapster configuration

---
+++ Backend/TodoList.Api/TodoList.Api/Mapping/MappingConfig.cs
@@ -0,0 +1,25 @@
+﻿using Mapster;
+using TodoList.Api.Models;
+using TodoList.Domain.Entities;
+
+namespace TodoList.Api.Mapping
+{
+    public static class MappingConfig
+    {
+        public static void RegisterMappings()
+        {
+            TypeAdapterConfig<TodoItemRequest, TodoItem>.NewConfig()
+                .Map(dest => dest.Id, src => src.Id)
+                .Map(dest => dest.Description, src => src.Description)
+                .Map(dest => dest.IsCompleted, src => src.IsCompleted);
+
+            TypeAdapterConfig<TodoItem, TodoItemResponse>.NewConfig()
+                .Map(dest => dest.Id, src => src.Id)
+                .Map(dest => dest.Description, src => src.Description)
+                .Map(dest => dest.IsCompleted, src => src.IsCompleted)
+                .Map(dest => dest.CreatedDate, src => src.CreatedDate)
+                .Map(dest => dest.CompletedDate, src => src.CompletedDate);
+        }
+
+    }
+}
